726ed2838724f51d801f8c1ac4ee38a29e890ff3
bitcoinerboy <hwasoonk57@gmail.com>
2026-06-12 00:10:27 +0700

    Switch production domain to automatorlabs.co

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://automator-labs.netlify.app',
+	site: 'https://automatorlabs.co',
 	integrations: [sitemap()],
 });
