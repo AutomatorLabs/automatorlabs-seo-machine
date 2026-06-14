aa5c99239bda8626e4fe5ca23ff8f932a5989dcb
bitcoinerboy <hwasoonk57@gmail.com>
2026-06-15 00:21:44 +0700

    Remove obsolete tools page

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://automatorlabs.co',
-	integrations: [
-		sitemap({
-			filter: (page) => page !== 'https://automatorlabs.co/tools/',
-		}),
-	],
+	integrations: [sitemap()],
 });
